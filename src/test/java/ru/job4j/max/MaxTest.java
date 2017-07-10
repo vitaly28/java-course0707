0d9c10037806f36adc04f62c8372e001b73ca748
Vitaly Berstenev <sm_plus82@mail.ru>
2017-07-10 14:14:20 +0400

    vitaly change

--- src/test/java/ru/job4j/max/MaxTest.java
+++ src/test/java/ru/job4j/max/MaxTest.java
@@ -1,4 +1,4 @@
-package com.company;
+package ru.job4j.max;
 
 import org.junit.Test;
 
@@ -8,11 +8,18 @@ import static org.junit.Assert.*;
 
 public class MaxTest {
               @Test
+    public void maxIsTwoNumber(){
+                  Max maxResultTest = new Max();
+                  int [] number = {1,2};
+                  int expected = 2;
+                  int result = maxResultTest.max(number [0],number [1]);
+                  assertThat(result, is (expected));
+    }
     public void maxIsTreeNumber(){
                   Max maxResultTest = new Max();
                   int [] number = {1,2,3};
                   int expected = 3;
                   int result = maxResultTest.max(number [0],number [1],number [2]);
                   assertThat(result, is (expected));
-              }
+    }
 }

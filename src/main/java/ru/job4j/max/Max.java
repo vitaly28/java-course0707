5ee9a17acb979929ea1b472edb5446d318eb7ee5
Vitaly Berstenev <sm_plus82@mail.ru>
2017-07-10 14:25:35 +0400

    vitaly change 14 25

--- src/main/java/ru/job4j/max/Max.java
+++ src/main/java/ru/job4j/max/Max.java
@@ -1,13 +1,7 @@
 package ru.job4j.max;
 
 public class Max {
-  public static void main(String[] args) {
-    Max cat = new Max();
-    Max cat1 = new Max();
-    System.out.println(cat.max(1,2));
-    System.out.println(cat.max(10,0,3));
-
-  }
+  
   public int max(int a, int b){
     if (a > b){return a;}
     else {return b;}

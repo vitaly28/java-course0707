a4b7514c7c4b6e271180888c61d5a122dbf7143d
Vitaly Berstenev <sm_plus82@mail.ru>
2017-07-12 15:30:46 +0400

    vitaly change 12 07 17 15:30

--- src/main/java/ru/job4j/max/Max.java
+++ src/main/java/ru/job4j/max/Max.java
@@ -1,11 +1,31 @@
 package ru.job4j.max;
-
+/**
+* Package for Max.
+*
+* @author Petr Arsentev (sm_plus82@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
 public class Max {
-  
+
+/**
+*Сравнение максимум
+*@param a первый аргумент
+*@param b второй аргумент
+*@return результат
+*/  
   public int max(int a, int b){
     if (a > b){return a;}
     else {return b;}
   }
+
+/**
+*Сравнение  максимум
+*@param a первый аргумент
+*@param b второй аргумент
+*@param c третий аргумент
+*@return результат
+*/
   public int max(int a, int b,int c){
 
     return max(max(a,b),c);
